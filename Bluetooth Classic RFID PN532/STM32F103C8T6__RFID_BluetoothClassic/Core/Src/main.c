805740015f8667a58c694b36d12f300aa107c103
Viktor Vaňo <viktorvano@gmail.com>
2020-03-14 18:08:00 +0100

    Removed a line of code

--- Bluetooth Classic RFID PN532/STM32F103C8T6__RFID_BluetoothClassic/Core/Src/main.c
+++ Bluetooth Classic RFID PN532/STM32F103C8T6__RFID_BluetoothClassic/Core/Src/main.c
@@ -114,7 +114,6 @@ int main(void)
 
   PN532 pn532;
   PN532_SPI_Init(&pn532);
-  PN532_GetFirmwareVersion(&pn532, buff);
   if (PN532_GetFirmwareVersion(&pn532, buff) != PN532_STATUS_OK)
   {
     while(1)
